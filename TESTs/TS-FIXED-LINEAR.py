c1954ceddf7c66161c6a681ee782f1439a2224bd
Pau Manyer Fuertes <p.manyer@hotmail.com>
2024-10-25 15:26:56 +0200

    Added developer information on test-suites files #main

--- TESTs/TS-FIXED-LINEAR.py
+++ TESTs/TS-FIXED-LINEAR.py
@@ -1,3 +1,32 @@
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Lesser General Public License for more details.
+
+# You should have received a copy of the GNU Lesser General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+# Author: Pau Manyer Fuertes
+# Email: pau.manyer@bsc.es
+# Date: October 2024
+# Institution: Barcelona Supercomputing Center (BSC)
+# Department: Computer Applications in Science and Engineering (CASE)
+# Research Group: Nuclear Fusion  
+
+# This script constitutes the test-suite for the FIXED-boundary plasma equilibrium problem 
+# where the plasma current is modelled using a LINEAR function. Such case yields an analytical 
+# solution, an therefore the exact numerical error can be computed and used for code validation. 
+
+# After selecting the MESH, the file may be executed to launch the solver. EQUILIPY's output can 
+# be turned ON and OFF by change the bolean output parameters.
+
+
+
 import sys
 sys.path.append('../')
 
